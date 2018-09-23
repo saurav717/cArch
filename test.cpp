4c3e3c12c78ef35d17ea5bdf5f7746fe0cf009cd
saurav <es16btech11007>
2018-09-23 09:23:03 +0530

    completed

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@
 #include<sstream>
 using namespace std;
 
-int search(string &dataS, vector<string> &My_Cache, int numberOfEntries, int &hit, int &miss, int &compulsory_misses, int &capacity_misses, vector<string> &dataI, string &replacement, vector<string> &data, vector<int> &maxvals, int &index)
+int search(string &dataS, vector<string> &My_Cache, int numberOfEntries, int &hit, int &miss, int &compulsory_misses, int &capacity_misses, vector<string> &dataI, string &replacement, vector<string> &data, vector<int> &maxvals, int &index, vector<string> &hitsAndMisses)
 {
   string temp;
   int indexD = 0, maxvalue = data.size()+5;
@@ -41,6 +41,7 @@ int search(string &dataS, vector<string> &My_Cache, int numberOfEntries, int &hi
     {
       hit = hit + 1;
       //cout << "hit\n";
+      hitsAndMisses.push_back("HIT");
       if(replacement == "OPTIMAL" || replacement == "optimal")
       {
         int j ;
@@ -71,11 +72,13 @@ int search(string &dataS, vector<string> &My_Cache, int numberOfEntries, int &hi
 //---------------------------------------------------------------------------
   if(indexD == 0)
   {
+    hitsAndMisses.push_back("MISS");
     compulsory_misses = compulsory_misses + 1;
   }
 
   else if(indexD == 1)
   {
+    hitsAndMisses.push_back("MISS");
     capacity_misses = capacity_misses + 1;
   }
 
@@ -113,12 +116,17 @@ int search(string &dataS, vector<string> &My_Cache, int numberOfEntries, int &hi
   }
 }
 
-void LRU(vector<string> &data, vector<string> &My_Cache, int numberOfEntries,string replacement)
+void LRU(vector<string> &data, vector<string> &My_Cache, int numberOfEntries,string replacement, string filename, string numberOfEntries2)
 {
 
   My_Cache.reserve(numberOfEntries);
-  vector<string> dataI;
+  vector<string> dataI, hitsAndMisses;
   vector<int> maxvals;
+  ofstream myfile;
+  string myfileName;
+  myfileName = "ES16BTECH11007_"+replacement+"_"+filename+"_"+numberOfEntries2+".txt";
+  myfile.open(myfileName.c_str());
+
   int hit = 0, miss = 0, compulsory_misses = 0, capacity_misses = 0, temp,count = 0;
   for(int i = 0; i<data.size(); i++)
   {
@@ -144,16 +152,22 @@ void LRU(vector<string> &data, vector<string> &My_Cache, int numberOfEntries,str
       {
           count++;
           int t = i+1;
-          search(data.at(i), My_Cache, numberOfEntries, hit, miss, compulsory_misses, capacity_misses, dataI, replacement, data, maxvals, t);
+          search(data.at(i), My_Cache, numberOfEntries, hit, miss, compulsory_misses, capacity_misses, dataI, replacement, data, maxvals, t, hitsAndMisses);
       }
 
   }
   miss = compulsory_misses + capacity_misses;
-  cout << "replacement policy - "<< replacement << "\n";
-  cout << "total accesses = " << hit + miss << "\n";
-  cout << "Total hits = " << hit << ", Total misses = " << miss << "\n";
-  cout << "compulsory misses = " << compulsory_misses << "\n";
-  cout << "capacity misses = " << capacity_misses << "\n";
+  myfile << "replacement policy - "<< replacement << "\n";
+  myfile << "total accesses = " << hit + miss << "\n";
+  //myfile << "Total hits = " << hit << ", Total misses = " << miss << "\n";
+  myfile << "Total misses = " << miss << "\n";
+  myfile << "compulsory misses = " << compulsory_misses << "\n";
+  myfile << "capacity misses = " << capacity_misses << "\n";
+  for(int i=0; i<hitsAndMisses.size(); i++)
+  {
+    myfile << hitsAndMisses[i] << "\n";
+  }
+  myfile.close();
 
 }
 
@@ -173,18 +187,16 @@ int main(int argc, char **argv)
 
   SS >> numberOfEntries;
 
-
-
   file.open(filename.c_str());
   int count = 0;
 
   while (file >> word)
   {
-    //  cout << word << endl;
+    ////  cout << word << endl;
       data.push_back(word);
   }
 
     //cout << "\n\n" <<"Vector(1) = " << data.size() << "\n";
   file.close();
-  LRU(data, My_Cache, numberOfEntries, replacement);
+  LRU(data, My_Cache, numberOfEntries, replacement,filename ,argv[3]);
 }
